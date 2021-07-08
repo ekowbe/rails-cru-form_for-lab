45036a8ae16ac83111a4907d06ebffabd1b24dc4
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-07-08 12:31:46 +0000

    artist part done

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,26 @@
+class ArtistsController < ApplicationController
+    def index
+        @artists = Artist.all
+    end
+
+    def create
+
+    end
+
+    def new
+
+    end
+
+    def edit
+
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+    
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create ten songs with the same artist
+artist = Artist.create(name: Faker::Artist.name, bio: Faker::Lorem.paragraph)
+10.times do
+    genre = Genre.create(name: Faker::GreekPhilosophers.name)
+    Song.create(name: Faker::FunnyName.name, artist_id: artist.id, genre_id: genre.id)
+
+end
+
+# create ten songs with ten more random artists
+
+10.times do
+    artist = Artist.create(name: Faker::Artist.name, bio: Faker::Lorem.paragraph)
+    genre = Genre.create(name: Faker::GreekPhilosophers.name)
+    Song.create(name: Faker::FunnyName.name, artist_id: artist.id, genre_id: genre.id)
+end
